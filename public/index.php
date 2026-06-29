a705bef980290501b4fc839152c6b935f4e2cc86
Wahyu Utomo <130083294+WagYu31@users.noreply.github.com>
2026-06-30 00:19:33 +0700

    fix: add Bootstrap JS bundle for announcement modal

--- public/index.php
+++ public/index.php
@@ -473,6 +473,6 @@ $tanggal = date('d M Y');
             });
     }
 </script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
